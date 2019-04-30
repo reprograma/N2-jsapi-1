503eccb759da64b4c24529a20d2ca403de3a3303
Isabelle Galvão <belle.galvao@yahoo.com.br>
2019-04-30 18:43:17 -0300

    add updates

--- exe-ghibli/script.js
+++ exe-ghibli/script.js
@@ -20,3 +20,5 @@ const obj = [
         "description": "It’s 1982, and Taeko is 27 years old, unmarried, and has lived her whole life in Tokyo. She decides to visit her family in the countryside, and as the train travels through the night, memories flood back of her younger years: the first immature stirrings of romance, the onset of puberty, and the frustrations of math and boys. At the station she is met by young farmer Toshio, and the encounters with him begin to reconnect her to forgotten longings. In lyrical switches between the present and the past, Taeko contemplates the arc of her life, and wonders if she has been true to the dreams of her childhood self."
      }
     ]
+
+    // COMEÇA O EXERCÍCIO
